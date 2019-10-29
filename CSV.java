526331c4d3a1b44b99b3cc1d2069d9f46be69d47
Constantin Irimia <cirimia49@gmail.com>
2019-10-28 21:15:25 -0500

    Add files via upload

---
+++ CSV.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class CSV{
+   public static List<String[]> read(String file){
+      List<String[]> data = new LinkedList<String[]>();
+      
+      String dataRow;
+      try {
+         
+         BufferedReader br = new BufferedReader(new FileReader(file));
+        
+        while(( dataRow = br.readLine()) != null ){
+            String[] dataRecords = dataRow.split(",");
+            data.add(dataRecords);
+         }
+         
+      } catch(FileNotFoundException ex){
+         System.out.println("Could not find file");
+         ex.printStackTrace();
+      } catch(IOException ex){ 
+         System.out.println("Could not read file");
+         ex.printStackTrace();
+      }
+      
+      return data;
+   }
+}
